baf61edd97f6b3b0a463d51dc8654082e1ebc8c4
Yooshyasha <mejzikrejzi66@gmail.com>
2026-04-22 22:04:04 +0500

    ref

--- backend/src/main/kotlin/com/yooshyasha/backend/config/VikunjaConfig.kt
+++ backend/src/main/kotlin/com/yooshyasha/backend/config/VikunjaConfig.kt
@@ -2,9 +2,9 @@ package com.yooshyasha.backend.config
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
-import org.springframework.stereotype.Component
+import org.springframework.context.annotation.Configuration
 
-@Component
+@Configuration
 class VikunjaConfig(
     @Value("\${vikunja.url}") private val vikunjaUrl: String,
     @Value("\${vikunja.token}") private val vikunjaToken: String,
