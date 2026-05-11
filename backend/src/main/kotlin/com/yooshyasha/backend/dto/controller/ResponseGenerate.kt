55342ae821d7cd24b622273cf55ae5d4ef09ab70
Yooshyasha <yooshyasha@proton.me>
2026-05-11 17:16:19 +0500

    feat

--- backend/src/main/kotlin/com/yooshyasha/backend/dto/controller/ResponseGenerate.kt
+++ backend/src/main/kotlin/com/yooshyasha/backend/dto/controller/ResponseGenerate.kt
@@ -7,12 +7,14 @@ import enum.TaskStatus
 
 data class ResponseGenerate(
     val status: TaskStatus,
+    val message: String? = null,
     val projectId: Int?,
     val generatedTasks: GeneratedTasksResponse?,
     val editMap: List<VikunjaTaskDTO>?,  // оригинал
 ) {
     constructor(response: ResponseGetTaskStatus, editMap: List<VikunjaTaskDTO>?, projectId: Int?) : this(
         status = response.status,
+        message = response.message,
         projectId = projectId,
         generatedTasks = response.generatedTasks,
         editMap = editMap,
