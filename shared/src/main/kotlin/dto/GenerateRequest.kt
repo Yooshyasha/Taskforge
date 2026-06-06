d4fbc320bee4900c9557b1b2e081e9d38be0117a
Yooshyasha <yooshyasha@proton.me>
2026-06-06 15:48:47 +0500

    feat

--- shared/src/main/kotlin/dto/GenerateRequest.kt
+++ shared/src/main/kotlin/dto/GenerateRequest.kt
@@ -1,8 +1,11 @@
 package dto
 
 import dto.project.VikunjaProjectDTO
+import enum.TaskDepth
 
 data class GenerateRequest(
     val text: String,
     val vikunjaProject: VikunjaProjectDTO?,
+    val language: String,
+    val taskDepth: TaskDepth,
 )
